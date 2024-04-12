1a52fe1b2409728655c34c24704ab5462a86fa8e
perelokesh <perelokesh7093@gmail.com>
2024-04-12 20:32:10 +0530

    changes

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="fixed top-32px left-32px">
+    <div className="w-406px h-49px p-26.65px bg-transparent font-semibold text-black font-montserrat">
+      THE <span className="bg-black text-white px-2">PRODUCT</span> PLATFORM
+    </div>
+  </div>
+  
+
+
+
+
+
+  )
+}
+
+export default Header
